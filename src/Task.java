ef4d585d7e020571475bc8d4b7ee2157087b652a
phumla <phmlotsjhb024@sudent.co.za>
2025-07-06 12:54:57 +0200

    added getter methods so other classes can access the values of descrption and isDone

--- src/Task.java
+++ src/Task.java
@@ -7,8 +7,16 @@ public class Task {
     public Task(String description){
         this.description = description;
         this.isDone = false;
-    
+
     }
+
+    public String getDescription(){
+        return description;
+    }
+    public boolean isDone(){
+        return isDone();
+    }
+
 
 
     
