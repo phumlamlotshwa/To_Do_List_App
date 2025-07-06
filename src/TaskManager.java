e805677dd026d87d99481dfd77c981789524a98b
phumla <phmlotsjhb024@sudent.co.za>
2025-07-06 13:33:10 +0200

    constructor

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -3,6 +3,13 @@ import java.util.List;
 public class TaskManager {
     private List<Task> tasks;
     private static final String FILE_NAME = "tasks.txt";
+
+
+    public TaskManager(){
+        tasks = new ArrayList<>();
+        loadTasks();
+
+    }
     
-    
+
 }
