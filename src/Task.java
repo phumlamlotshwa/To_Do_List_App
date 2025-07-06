a24317d1b4419b51332ca0b3a64ebb4867bccceb
phumla <phmlotsjhb024@sudent.co.za>
2025-07-06 12:52:09 +0200

    added a constructor

--- src/Task.java
+++ src/Task.java
@@ -1,10 +1,14 @@
-
+import java.nio.file.FileAlreadyExistsException;
 
 public class Task {
     private String description;
     private String isDone;
 
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
     
+    }
 
 
     
